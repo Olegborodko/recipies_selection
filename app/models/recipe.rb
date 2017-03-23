b9246683a6ce34c3e5dc8946165f73cc44442842
dmitriy.sanitskiy <dmitriy.sanitskiy@gmail.com>
2017-03-23 14:06:41 +0200

    reformat code

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,31 @@
 class Recipe < ApplicationRecord
   belongs_to :recipe_category
-  has_many :ingredients
 
-  has_many :favoriterecipes
-  has_many :users, :through => :favoriterecipes
+  has_many :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
+
+  has_many :favorite_recipes
+  # has_many (or has_and_belongs_to_many):users, :through => :favorite_recipes    <- need create join table recipes_users
+
+  include PgSearch
+  pg_search_scope :search, against: [:name, :content],
+                  using: {tsearch: {dictionary: "english"}},
+                  associated_against: {recipe_category: :title, ingredients: [:name, :content],
+                                       # users: [:name, :email, :slug]
+                  },
+                  ignoring: :accents
+
+
+  def self.text_search(query)
+    if query.present?
+      # search(query)
+      rank = <<-RANK
+        ts_rank(to_tsvector(name), plainto_tsquery(#{sanitize(query)})) + 
+        ts_rank(to_tsvector(content), plainto_tsquery(#{sanitize(query)}))
+      RANK
+      where("to_tsvector('english', name) @@ :q or to_tsvector('english', content) @@ :q", q: query).order("#{rank} desk")
+    else
+      scoped
+    end
+  end
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,5 @@
 class Role < ApplicationRecord
   has_many :users
+
+  validates :title, presence: true, length: {minimum: 2, maximum: 100}
 end
