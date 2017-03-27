a0b466637af086d7e099d4ff164d031f7f6fb6f5
dmitriy.sanitskiy <dmitriy.sanitskiy@gmail.com>
2017-03-27 13:11:13 +0300

    pg_search_V2 work like commit pg_search#2

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ApplicationRecord
+  include PgSearch
+
   belongs_to :recipe_category
 
   has_many :recipe_ingredients
@@ -6,4 +8,8 @@ class Recipe < ApplicationRecord
 
   has_many :favorite_recipes
   has_many :users, :through => :favorite_recipes
+
+  pg_search_scope :search, :against => [:name, :content],
+                  :associated_against => { ingredients: [:name, :content] },
+                  :using => [:tsearch, :trigram, :dmetaphone, :tsearch => {:any_word => true}]
 end
